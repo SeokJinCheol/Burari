b95010a6536548360924c8d7047e74622e366abe
석진철 <tjrwlscjf13@gmail.com>
2019-08-20 14:50:48 +0900

    create spring boot

---
+++ src/main/java/com/project/burari/BurariApplication.java
@@ -0,0 +1,13 @@
+package com.project.burari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurariApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/burari/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.project.burari.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MainController {
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ModelAndView main(ModelAndView mv) {
+		
+		System.out.println("main controller");
+			
+		return mv;
+	}
+}
