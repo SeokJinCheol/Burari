c3a2654c78afaa2005c78b77152f6fbc655225fa
석진철 <tjrwlscjf13@gmail.com>
2019-09-08 05:22:41 +0900

    index.jsp redirect react-app

--- src/main/java/com/project/burari/controller/MainController.java
+++ src/main/java/com/project/burari/controller/MainController.java
@@ -1,17 +1,24 @@
 package com.project.burari.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.net.InetAddress;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
 	
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public ModelAndView main(ModelAndView mv) {
-		mv.setViewName("index");
+	@GetMapping(value="/")
+	public ModelAndView main(ModelAndView mv) throws Exception {
+		
+		InetAddress local = InetAddress.getLocalHost();
+		String ip = local.getHostAddress();
+        
+        mv.setViewName("index");
+        mv.addObject("clientIP", ip);
+		
+		System.out.println(ip);
 		return mv;
 	}
 }
--- src/main/java/com/project/burari/controller/ProjectController.java
+++ src/main/java/com/project/burari/controller/ProjectController.java
@@ -4,11 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.project.burari.core.controller.BaseController;
