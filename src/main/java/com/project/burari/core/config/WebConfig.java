b6b76b61b49c7f8fbf6277ef060d95240f1b1c3f
석진철 <tjrwlscjf13@gmail.com>
2019-09-09 22:32:35 +0900

    cross config update

--- src/main/java/com/project/burari/core/config/WebConfig.java
+++ src/main/java/com/project/burari/core/config/WebConfig.java
@@ -1,13 +1,25 @@
 package com.project.burari.core.config;
 
+import java.net.InetAddress;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+	
+	String ip;
+	
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:8080","http://localhost:3000");
+	public void addCorsMappings(CorsRegistry registry){
+
+		
+		registry.addMapping("/**").allowedOrigins("http://"+ this.ip +":8080", "http://" + this.ip + ":3000").allowedOrigins("http://localhost:8080", "http://localhost:3000");
+	}
+	
+	public void returnIP() throws Exception{
+		InetAddress local = InetAddress.getLocalHost();
+		this.ip = local.getHostAddress();
 	}
 }
